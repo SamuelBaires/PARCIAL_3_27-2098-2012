906a3538369968f1c90ec2fcfa1f0885f61b11b8
SamuelBaires <47999891+SamuelBaires@users.noreply.github.com>
2019-04-23 19:56:58 -0600

    Add files via upload

---
+++ Login/Login/Formulario3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Formulario3 : Form
+    {
+        public Formulario3()
+        {
+            InitializeComponent();
+        }
+
+        private void Formulario3_Load(object sender, EventArgs e)
+        {
+            txtName.Text = "Samuel Alexander Baires";
+            txtCarnet.Text = "27-2098-2012";
+            txtMateria.Text="Programacion 1";
+            txtciclo.Text="01-2019";
+    }
+}
+}
---
+++ Login/Login/Formulario4.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Login
+{
+    partial class Formulario4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtCuantopor = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.rdbAgregar = new System.Windows.Forms.RadioButton();
+            this.rdbNoagregar = new System.Windows.Forms.RadioButton();
+            this.txtPorcentaje = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cantidad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tanto %";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Opciones de porcentaje";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Porciento total";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Total";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(21, 29);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 6;
+            // 
+            // txtCuantopor
+            // 
+            this.txtCuantopor.Location = new System.Drawing.Point(185, 29);
+            this.txtCuantopor.Name = "txtCuantopor";
+            this.txtCuantopor.Size = new System.Drawing.Size(100, 20);
+            this.txtCuantopor.TabIndex = 7;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(117, 107);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 8;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(264, 75);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // rdbAgregar
+            // 
+            this.rdbAgregar.AutoSize = true;
+            this.rdbAgregar.Location = new System.Drawing.Point(50, 152);
+            this.rdbAgregar.Name = "rdbAgregar";
+            this.rdbAgregar.Size = new System.Drawing.Size(119, 17);
+            this.rdbAgregar.TabIndex = 10;
+            this.rdbAgregar.TabStop = true;
+            this.rdbAgregar.Text = "Agregar al resultado";
+            this.rdbAgregar.UseVisualStyleBackColor = true;
+            // 
+            // rdbNoagregar
+            // 
+            this.rdbNoagregar.AutoSize = true;
+            this.rdbNoagregar.Location = new System.Drawing.Point(53, 193);
+            this.rdbNoagregar.Name = "rdbNoagregar";
+            this.rdbNoagregar.Size = new System.Drawing.Size(135, 17);
+            this.rdbNoagregar.TabIndex = 11;
+            this.rdbNoagregar.TabStop = true;
+            this.rdbNoagregar.Text = "No agregar al resultado";
+            this.rdbNoagregar.UseVisualStyleBackColor = true;
+            // 
+            // txtPorcentaje
+            // 
+            this.txtPorcentaje.Location = new System.Drawing.Point(117, 81);
+            this.txtPorcentaje.Name = "txtPorcentaje";
+            this.txtPorcentaje.Size = new System.Drawing.Size(100, 20);
+            this.txtPorcentaje.TabIndex = 12;
+            // 
+            // Formulario4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 222);
+            this.Controls.Add(this.txtPorcentaje);
+            this.Controls.Add(this.rdbNoagregar);
+            this.Controls.Add(this.rdbAgregar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtCuantopor);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Formulario4";
+            this.Text = "Formulario4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtCuantopor;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.RadioButton rdbAgregar;
+        private System.Windows.Forms.RadioButton rdbNoagregar;
+        private System.Windows.Forms.TextBox txtPorcentaje;
+    }
+}
